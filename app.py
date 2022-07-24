f1facc54d732905cd12c27bcfd10269717769199
Agustin <agustinbraida29@gmail.com>
2022-07-23 23:06:07 -0300

    Se sube todo el codigo. Primer commit

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print('Hola mundo')
+# Libreria para comprimir imagenes
+from PIL import Image 
+# Libreria que nos permite leer los archivos
+import os
+
+
+# Definimos la carpeta en donde estan los archivos
+descargaArchivos = '/Users/agust/OneDrive/Imágenes del celular/Camara Compu/'
+
+if __name__ == '__main__':
+    for nombre in os.listdir(descargaArchivos):
+        name, extension = os.path.splitext(descargaArchivos + nombre)
+
+        # firltramos por la extencion
+        if extension in ['.jpg', '.jpeg', '.png']:
+            imagen = Image.open(descargaArchivos + nombre)
+            imagen.save(descargaArchivos + 'Comprimido_' + nombre, optimize=True, quality=60)
+
